9ba71643cf3129a292ce7d64dffbe61b6b866ba0
+ <+>
2022-12-07 16:53:03 +1100

    commit tree eff575014a4af96636f49aee4a929893ee71901f

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    return Math.abs(block-42);
+
+}
+function distanceFromHqInFeet(feet){
+    if (feet > 42)
+    return ( feet -42) * 264;
+    else if (feet < 42)
+    return( 42 - feet) * 264;
+}
+function distanceTravelledInFeet(start, ending){
+    if ( start > ending)
+    return (start - ending) * 264
+
+    else if (ending > start)
+    return (ending - start) * 264
+
+
+}
+    function calculatesFarePrice(start, destination){
+        if (start, destination)
+        return'free ride'
+    
+      
+    }
+
+    
